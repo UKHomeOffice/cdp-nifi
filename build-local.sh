94b7d5a55139fdfe3493daae6f4e31be8dcc3b12
Pontus <contrib@pontusnetworks.com>
2018-02-18 00:11:39 +0000

    adding flowfile

--- build-local.sh
+++ build-local.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -x
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 VERSION=1.4.0
 echo DIR is $DIR
 export DISTDIR="$DIR/../pontus-dist/opt/pontus/pontus-nifi";
@@ -17,6 +17,7 @@ cd $DISTDIR
 
 rm -rf *
 tar xvf $DIR/nifi-assembly/target/nifi-${VERSION}-bin.tar.gz
+cp $DIR/flow.xml.gz $DISTDIR/nifi-${VERSION}/conf/
 
 ln -s nifi-${VERSION} current
 cd $CURDIR
